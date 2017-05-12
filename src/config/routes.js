9f34a56d592e0c7f13a42411d1eee2ed231dfa81
vol4tim <vol4tim@gmail.com>
2017-05-12 15:06:34 +0400

    remove start

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,14 +2,12 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from '../shared/containers/app'
 import NotFound from '../shared/components/app/notFound'
-import { Start } from '../routes/start'
 import * as Crowdfunding from '../routes/crowdfunding'
 
 export const routes = () =>
   (<div>
     <Route path="/" component={App}>
       <IndexRoute component={Crowdfunding.Main} />
-      <Route path="/change-market" component={Start} />
     </Route>
     <Route path="*" component={NotFound} />
   </div>)
